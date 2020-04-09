9fecdf390cf382a34ca416fc1f0a5243cecf83e2
Kevin Kihara <kevoh@nexttech>
2020-04-09 14:58:57 +0300

    removed unnecessarly declarations

--- src/estimator.js
+++ src/estimator.js
@@ -1,30 +1,3 @@
-const impact = {
-  currentlyInfected: 0,
-  infectionsByRequestedTime: 0,
-  infectedPeoplePerDay: 0,
-  infectedPeopleOver30Days: 0
-};
-const severeImpact = {
-  currentlyInfected: 0,
-  infectionsByRequestedTime: 0,
-  infectedPeoplePerDay: 0,
-  infectedPeopleOver30Days: 0
-};
-const data = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-
-
 const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
@@ -67,6 +40,4 @@ const covid19ImpactEstimator = (data) => {
   };
 };
 
-
-covid19ImpactEstimator(data);
 export default covid19ImpactEstimator;
