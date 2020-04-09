eaa025336db8a565b32e95c46cd91eb35cbb67c0
Kevin Kihara <kevoh@nexttech>
2020-04-09 12:36:47 +0300

    added few changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,15 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    
+const input =   data;
+return {
+    data:input, // the input data you got
+    impact: {}, // your best case estimation
+    severeImpact: {} // your severe case estimation
+   }
+
+}
+
+
+
 
 export default covid19ImpactEstimator;
