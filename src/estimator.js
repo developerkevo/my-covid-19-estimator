121042e01e6cde47b0c5becd15aa99d4850b791c
Kevin Kihara <kevoh@nexttech>
2020-04-09 13:02:48 +0300

    fixed errors on linting

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,9 @@
-const covid19ImpactEstimator = ( data ) => {
-    const input = data;
-    return {
-        data: input, // the input data you got
-        impact: {}, // your best case estimation
-        severeImpact: {} // your severe case estimation
-    }
-
-}
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input, // the input data you got
+    impact: {}, // your best case estimation
+    severeImpact: {} // your severe case estimation
+  };
+};
 export default covid19ImpactEstimator;
