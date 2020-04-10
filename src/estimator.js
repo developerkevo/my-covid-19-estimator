b552a2cf1f0856b9575fd38501294d1ca1c0c144
Kevin Kihara <kevoh@nexttech>
2020-04-10 23:13:26 +0300

    fix 16

--- src/estimator.js
+++ src/estimator.js
@@ -13,11 +13,11 @@ const covid19ImpactEstimator = (data) => {
   } else if (data.periodType === 'months') {
     days = 30 * data.timeToElapse;
   }
-  const factor = Math.trunc(days / 3);
+  const factor = 2 ** Math.trunc(days / 3);
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  impact.infectionsByRequestedTime = impact.currentlyInfected * factor;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * factor;
   impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
   severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
 
@@ -28,7 +28,7 @@ const covid19ImpactEstimator = (data) => {
   impact.casesForICUByRequestedTime = (0.05 * impact.infectionsByRequestedTime);
   severeImpact.casesForICUByRequestedTime = (0.05 * severeImpact.infectionsByRequestedTime);
 
-  impact.casesForVentilatorsByRequestedTime = (0.02 * impact.infectionsByRequestedTime).toFixed(2);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
   severeImpact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
 
   const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
