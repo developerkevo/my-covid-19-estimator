b010fa87f1651ab48b7bc65ca932791122e1b702
Kevin Kihara <kevoh@nexttech>
2020-04-10 12:19:06 +0300

    tried tofix a decimal error

--- src/estimator.js
+++ src/estimator.js
@@ -22,13 +22,13 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
 
   const severecases = severeImpact.severeCasesByRequestedTime;
-  impact.hospitalBedsByRequestedTime = (beds - impact.severeCasesByRequestedTime);
-  severeImpact.hospitalBedsByRequestedTime = (beds - severecases);
+  impact.hospitalBedsByRequestedTime = Math.trunc(beds - impact.severeCasesByRequestedTime);
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc(beds - severecases);
 
   impact.casesForICUByRequestedTime = (0.05 * impact.infectionsByRequestedTime);
   severeImpact.casesForICUByRequestedTime = (0.05 * severeImpact.infectionsByRequestedTime);
 
-  impact.casesForVentilatorsByRequestedTime = (0.02 * impact.infectionsByRequestedTime);
+  impact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * impact.infectionsByRequestedTime);
   severeImpact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
 
   const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
